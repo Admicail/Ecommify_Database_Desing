b38194c918cd6441ffbefaacde9acf92a5a250a4
EstebanGarayCano <99844206+EstebanGarayCano@users.noreply.github.com>
2026-06-15 09:00:04 -0500

    Update Index.sql

--- mongodb/Queries/Index/Index.sql
+++ mongodb/Queries/Index/Index.sql
@@ -2,3 +2,14 @@ db.CATALOG_PRODUCTS_COLLECTION.createIndex({
   category: 1, 
   "active_promotions.discount": -1 
 })
+
+// Índices principales.
+db.catalog_products_collection.createIndex({ category: 1 });
+db.catalog_products_collection.createIndex({ "reviews_summary.avg_score": -1 });
+db.catalog_products_collection.createIndex({ "active_promotions.is_promo": 1 });
+db.catalog_products_collection.createIndex({ "active_promotions.valid_until": 1 });
+db.catalog_products_collection.createIndex({ postgres_product_id: 1 });
+
+db.customer_sessions_collection.createIndex({ customer_id: 1 });
+db.customer_sessions_collection.createIndex({ last_activity: -1 });
+db.customer_sessions_collection.createIndex({ expires_at: 1 }, { expireAfterSeconds: 0 });
